8daa483507eecdada34db3a75eee5a06536a2c33
Prahlad Yeri <prahladyeri@yahoo.com>
2025-01-01 11:17:03 +0530

    chore: updated markdown, fixes

--- dict/Program.cs
+++ dict/Program.cs
@@ -10,20 +10,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Reflection;
+using System.IO;
 
 namespace dict
 {
     class Program
     {
-        // @todo: copy to current location
-        static string wnPath = @"D:\source\data\wordnet-princeton.edu-wn3.1.dict\";
+        static string wnPath = AppDomain.CurrentDomain.BaseDirectory + @"wn.dict\";
         static string indexFilePath = wnPath + "index.noun";
         static string dataFilePath = wnPath + "data.noun";
 
         static List<WordIndexEntry> indexEntries;
         static Dictionary<int, Synset> synsets;
 
-        private static void lookUp(string wordToLookup) { 
+        private static void lookUp(string wordToLookup) {
             WordIndexEntry entry = indexEntries.Find(e => e.Word == wordToLookup);
             if (entry != null)
             {
@@ -46,7 +46,7 @@ namespace dict
         
         }
 
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
             string attrib = "This software uses WordNet® lexical database (http://wordnet.princeton.edu/) by Princeton University.";
             indexEntries = WordNetIndexReader.ReadIndexFile(indexFilePath);
@@ -55,10 +55,6 @@ namespace dict
             Version v= Assembly.GetExecutingAssembly().GetName().Version;
             string version= string.Format("{0}.{1}", v.Major, v.Minor);
             Console.WriteLine("Dict, version " + version );
-            Console.WriteLine("Copyright (C) 2024 Prahlad Yeri <prahladyeri@yahoo.com>");
-            Console.WriteLine("License: MIT");
-            Console.WriteLine("This is free software; you are free to change and redistribute it.");
-            Console.WriteLine("There is NO WARRANTY, to the extent permitted by law.\n");
             
             if (args.Length == 0)
             {
@@ -68,8 +64,12 @@ namespace dict
             lookUp(args[0]);
             //lookUp("dog");
             //Console.ReadKey();
-
+            Console.WriteLine("");
+            Console.WriteLine("Copyright (C) 2024 Prahlad Yeri <prahladyeri@yahoo.com>");
+            Console.WriteLine("License: MIT");
             Console.WriteLine(attrib);
+            Console.WriteLine("This is free software; you are free to change and redistribute it.");
+            Console.WriteLine("There is NO WARRANTY, to the extent permitted by law.\n");
         }
     }
 }
